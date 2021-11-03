27deaf31f440c6f566ca34eeee5175a9d8f7eac7
Ricky <ricardo.cimarelli@gmail.com>
2021-11-03 12:05:42 -0300

    adding bad state logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,24 +22,31 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var random = Random();
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 1;
     return Center(
       child: Center(
         child: Row(
           children: [
             Expanded(
               child: TextButton(
-                child: Image(image: AssetImage("images/dice1.png")),
+                child:
+                    Image(image: AssetImage("images/dice$leftDiceNumber.png")),
                 onPressed: () {
                   print("Button 1 pressed");
+                  leftDiceNumber = random.nextInt(6);
                 },
               ),
               flex: 1,
             ),
             Expanded(
               child: TextButton(
-                child: Image(image: AssetImage("images/dice2.png")),
+                child:
+                    Image(image: AssetImage("images/dice$rightDiceNumber.png")),
                 onPressed: () {
                   print("Button 2 pressed");
+                  rightDiceNumber = random.nextInt(6);
                 },
               ),
               flex: 1,
